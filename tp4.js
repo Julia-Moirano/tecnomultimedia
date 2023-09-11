b6626f22593bd195d1b0a366c95642b409e43350
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2023-09-10 23:43:09 -0300

    Abreviación de funciones en relación al cuerpo de los jugadores + Reloj hecho desde formato de texto

--- tp4.js
+++ tp4.js
@@ -5,13 +5,20 @@
 let luchadorx, luchadory;
 let rivalx, rivaly;
 let ancho, altura;
+let blanco= 255;
+let negro= 0;
 let estado= 0;
 let puntaje1= 0;
 let puntaje2= 0;
-let segundos;
-let minutos;
-let arranque;
+let tiempototal= 91;
 let imagen;
+let temporarizador = new Array(tiempototal);
+let arranque;
+
+function preload(){
+  imagen = loadImage("data/imagen0.png");
+  temporarizador= loadStrings("data/temporarizador.txt");
+}
 
 function setup() {
  createCanvas(800,600);
@@ -21,25 +28,19 @@ function setup() {
   rivaly= height/2;
   ancho= 50;
   altura= 50;
-  //Cómo la pelea dura un minuto y medio, empezará con las siguentes variables númericas:
-  segundos=30;
-  minutos=1;
-}
-
-function preload(){
-  imagen = loadImage("data/imagen0.png");
 }
 
 function draw() {
+  if(estado===0){
+    background(175,255,127);
+    text("hi, this is boxing. Lets play!", 200,200);
+  } else if(estado===1){
+    
+ if(arranque===true){   
  if(frameCount%60===0){
-   segundos--;
-   if(segundos<10 && segundos>=0){
-     segundos="0"+segundos;
-   } else if(segundos<0){
-     minutos= 0;
-     segundos= 59;
-   }
-  }
+   tiempototal--;
+ }
+ }
   background(175,255,127);
   rectMode(CENTER);
   textAlign(CENTER,CENTER);
@@ -49,6 +50,8 @@ function draw() {
   stroke(150,75,0);
   rect(width/2,height/2,600,450);
   pop();
+  //luchador(luchadorx,luchadory,ancho,altura);
+  //rival(rivalx,rivaly,ancho,altura);
     if (keyIsDown(87)||keyIsDown(119)) {
       if (luchadory>450/3) {
       luchadory-=2;
@@ -86,7 +89,7 @@ function draw() {
     push();
     translate(luchadorx,luchadory);
     rotate(PI/4);
-    brazosdeluchador(0,0,50,50);
+    brazosdejugador1(0,0,255);
     pop();
     if (colision(luchadorx,luchadory,rivalx,rivaly,ancho,altura)){
       fill(200,200,15);
@@ -96,77 +99,102 @@ function draw() {
     push();
     translate(luchadorx,luchadory);
     rotate(7*PI/4);
-    brazosdeluchador(0,0,50,50);
+    brazosdejugador1(0,0,255);
     pop();
   } else {
-    brazosdeluchador(luchadorx,luchadory);
+    brazosdejugador1(luchadorx,luchadory,blanco);
   }
-  cabezadeluchador(luchadorx,luchadory,50,50);
-  
+  cabeza(luchadorx,luchadory,50,50,blanco);
   ///////////////////////////////////////////////
   if(keyIsDown(76)||keyIsDown(108)){
     push();
     translate(rivalx,rivaly);
     rotate(7*PI/4);
-    brazosderival(25,0,50,50);
+    brazosdejugador2(0,0,0);
     pop();
   } else if(keyIsDown(75)||keyIsDown(107)){
     push();
     translate(rivalx,rivaly);
     rotate(PI/4);
-    brazosderival(25,0,50,50);
+    brazosdejugador2(0,0,0);
     pop();
   } else {
-  brazosderival(rivalx+25,rivaly);
+  brazosdejugador2(rivalx,rivaly,negro);
   }
-  cabezaderival(rivalx,rivaly,50,50);
+  cabeza(rivalx,rivaly,50,50,negro);
    image(imagen,0,0);
+  push();
    fill(127,0,0);
    textSize(30);
    text(puntaje1,width/5,height/15);
    text(puntaje2,width/5+480,height/15);
    textSize(40);
-   temporarizador("0"+minutos+":"+segundos,width/2,height/12);
+   text(temporarizador[tiempototal],width/2,height/12);
    pop();
+ if(tiempototal<1){
+  arranque=false;
+  fill(175,255,127,125);
+  rect(width/2,height/2,800,600);
+  fill(127,50,0);
+  text("you won yipiii", 200,200);
+  boton(width/4,height/4+height/2,100,50,0);
+}
+  }
+}
+function mousePressed(){
+ if(estado===0){
+    arranque=true;
+    estado=1;
+    tiempototal=91;
+  } else if(estado===1){
+   if(pulsaboton(width/4,height/4+height/2,100,50,0)){
+    estado=0;
+  }
+  }
 }
 
-function brazosdeluchador(posx,posy){
+function brazosdejugador1(posx,posy,valor){
   push();
-  fill(255);
-  noStroke();
+  fill(valor);
+  stroke(valor);
   rect(posx-12.5,posy,50-25,50+100);//brazos
   rect(posx+25,posy+62.5,50,50-25);//puños
   rect(posx+25,posy-62.5,50,50-25);//puños
   pop();
 }
 
-function cabezadeluchador(posx,posy,anchura,altura){
+function brazosdejugador2(posx,posy,valor){
   push();
-  fill(255);
+  translate(posx,posy);
+  rotate(2*PI/2);
+  fill(valor);
+  stroke(valor);
+  noStroke();
+  brazosdejugador1(0,0,valor);
+  pop();
+}
+
+function cabeza(posx,posy,anchura,altura,valor){
+  push();
+  fill(valor);
   stroke(125);
   ellipse(posx,posy,anchura,altura);
   pop();
 }
 
-function brazosderival(posx,posy){
+function boton(posx,posy,anchura,altura,radio){
   push();
-  fill(0);
-  stroke(0);
-  rect(posx-12.5,posy,50-25,50+100);//brazos
-  rect(posx-50,posy+62.5,50,50-25);//puños
-  rect(posx-50,posy-62.5,50,50-25);//puños
+  rectMode(CORNER);
+  if(pulsaboton(posx,posy,anchura,altura)){
+    fill(60,60,95);
+  } else {
+    fill(60,60,95,125);
+  }
+  stroke(15,15,15);
+  rect(posx,posy,anchura,altura,radio);
   pop();
 }
-function cabezaderival(posx,posy,anchura,altura){
-  push();
-  fill(0);
-  stroke(125);
-  ellipse(posx,posy,anchura,altura);
-  pop();
-}
-function temporarizador(texto,ancho,altura){
-  text(texto,ancho,altura);
-}
+
 function colision(posx1,posy1,posx2,posy2,ancho2,altura2){
   let distancia= dist(posx1,posy1,posx2,posy2);
   if(distancia<ancho2 && distancia<altura2){
@@ -175,5 +203,11 @@ function colision(posx1,posy1,posx2,posy2,ancho2,altura2){
     return false;
   }
 }
+
+function pulsaboton(posx,posy,anchura,altura){
+  if(mouseX>posx && mouseX<posx+anchura && mouseY>posy && mouseY<posy+altura){
+    return true;
+  } else {
+    return false;
   }
 }
