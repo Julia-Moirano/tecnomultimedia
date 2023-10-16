79913e3bc60590460063ec0bfa63e134ed1ec732
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2023-10-16 20:39:29 -0300

    Update personaje.js

--- personaje.js
+++ personaje.js
@@ -1,19 +1,38 @@
 class personaje{
-  constructor(){
-  }
-  
-  dibujarPersonaje(perx,pery){
+   constructor(perx,pery){
     this.perx= perx;
     this.pery= pery;
-    image(imagenes[13],perx,pery);
   }
   
-   moverDerecha(){
-    this.perx += 100;
-   // this.posy+=50;
+  dibujarPersonaje1(perpresente,persiguiente){
+    this.perpresente=perpresente;
+    this.persiguiente=persiguiente;
+    image(imagenes[13],this.perx,this.pery);
   }
-   moverIzquierda(){
-    this.perx -= 100;
-   // this.posy-=50;
+  
+  dibujarPersonaje2(perpresente,persiguiente){
+    this.perpresente=perpresente;
+    this.persiguiente=persiguiente;
+    image(imagenes[14],this.perx,this.pery);
+  }
+  
+  moverIzquierda1(){
+    this.perx-=100;
+    this.pery-=50;
+  }
+  moverDerecha1(){
+    this.perx+=100;
+    this.pery+=50;
+  }
+  
+  moverIzquierda2(){
+    this.perx-=100;
+    //console.log(this.perx);
+    this.pery+=50;
+  }
+  moverDerecha2(){
+    this.perx+=100;
+    //console.log(this.perx);
+    this.pery-=50;
   }
 }
