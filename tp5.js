77834dfa2c53062e12a9bba7eafb0b1e5f1ffde1
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2023-10-21 01:37:58 -0300

    Update tp5.js

--- tp5.js
+++ tp5.js
@@ -3,19 +3,13 @@
 // Link del video:
 
 let principal;
-let cantidadtotal= 21;
-let imagenes= new Array(cantidadtotal);
 
 function setup() {
  createCanvas(800,600);
  principal= new juego();
- for (let i= 0; i < cantidadtotal; i++) {
-   imagenes[i]= loadImage("data/imagen"+i+".png");
- }
 }
 
 function draw() {
-  print(mouseX);
   background(0);
   principal.dibujar();
 }
