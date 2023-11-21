88b6c42adfba2759077239d26e8556d49484a036
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2023-11-20 23:49:28 -0300

    Update clasePulsador.js

--- clasePulsador.js
+++ clasePulsador.js
@@ -1,19 +1,18 @@
 class boton {
-  constructor(pulx, puly, pulancho, pulalto) {
+  constructor() {
+  }
+
+  dibujar(pulx, puly, pulancho, pulalto, texto, pulpresente, pulsiguiente) {
+    push();
     this.pulx= pulx;
     this.puly= puly;
     this.pulancho= pulancho;
     this.pulalto= pulalto;
     textAlign(CENTER, CENTER);
     textSize(this.pulalto/2);
-  }
-
-  dibujar(texto, pulpresente, pulsiguiente) {
-    push();
     rectMode(CENTER);
     this.pulpresente= pulpresente;
     this.pulsiguiente= pulsiguiente;
-    //aventura.pantallaActual = pulpresente;
     this.texto= texto;
     fill(125, 125, 175);
     rect(this.pulx, this.puly, this.pulancho, this.pulalto, 15);
@@ -22,7 +21,7 @@ class boton {
     pop();
   }
 
-  apretar(pulx, puly, pulancho, pulalto) {
+  apretar() {
     return mouseX>this.pulx-this.pulancho/2 && mouseX<this.pulx+this.pulancho/2 && mouseY>this.puly-this.pulalto/2 && mouseY<this.puly+this.pulalto/2;
   }
 }
