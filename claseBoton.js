b8bcf6f448a7930ec204f2078ee9b9278ecd67b0
Julia-Moirano <128604705+Julia-Moirano@users.noreply.github.com>
2023-12-04 19:48:41 -0300

    Update claseBoton.js

--- claseBoton.js
+++ claseBoton.js
@@ -4,16 +4,16 @@ class boton {
 
   dibujar(pulx, puly, pulancho, pulalto, texto, pulpresente, pulsiguiente) {
     push();
-    this.pulx= pulx;
-    this.puly= puly;
-    this.pulancho= pulancho;
-    this.pulalto= pulalto;
+    this.pulx = pulx;
+    this.puly = puly;
+    this.pulancho = pulancho;
+    this.pulalto = pulalto;
     textAlign(CENTER, CENTER);
-    textSize(this.pulalto/2);
+    textSize(this.pulalto / 2);
     rectMode(CENTER);
-    this.pulpresente= pulpresente;
-    this.pulsiguiente= pulsiguiente;
-    this.texto= texto;
+    this.pulpresente = pulpresente;
+    this.pulsiguiente = pulsiguiente;
+    this.texto = texto;
     fill(125, 125, 175);
     rect(this.pulx, this.puly, this.pulancho, this.pulalto, 15);
     fill(90, 60, 90);
@@ -22,6 +22,6 @@ class boton {
   }
 
   apretar() {
-    return mouseX>this.pulx-this.pulancho/2 && mouseX<this.pulx+this.pulancho/2 && mouseY>this.puly-this.pulalto/2 && mouseY<this.puly+this.pulalto/2;
+    return mouseX > this.pulx - this.pulancho / 2 && mouseX < this.pulx + this.pulancho / 2 && mouseY > this.puly - this.pulalto / 2 && mouseY < this.puly + this.pulalto / 2;
   }
 }
